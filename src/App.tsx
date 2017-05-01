3ebecebf8c4b064f95d647334e072eeab13457aa
Brian Lee <brianjleeofcl@gmail.com>
2017-05-01 11:41:08 -0700

    Add class definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, IndexRoute } from 'react-router-dom';
 import './App.css';
 
-const logo = require('./logo.svg');
+// const logo = require('./logo.svg');
 
 class App extends React.Component<{}, null> {
   render() {
     return (
       <div className="App">
         <div className="App-header">
+
           <h1>2048</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Router>
+          <Route path="/" component={Wrapper}>
+            <IndexRoute component={GameBoard} />
+            <Route path="/scores" component={HighScores} />
+          </Route>
+        </Router>
       </div>
     );
   }
